12b134d94a8f139645c2384c3c24654eb59f0a8f
ChiefMigz <pebenitomiguel@gmail.com>
2022-04-14 13:18:27 -0700

    Implemented calculate function which shows the weight progresion after given months in entry

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 from ctypes import alignment
 import tkinter
+import tkinter.messagebox
 
 class CalorieCalulator:
     def __init__(self):
@@ -13,8 +14,8 @@ class CalorieCalulator:
         self.buttonWindow = tkinter.Frame(self.mainWindow)
         
         #Entry Weight Entry
-        self.startingWeightLabel = tkinter.Label(self.topWindow, text="Starting weight: ")
-        self.startingWeightEntry = tkinter.Entry(self.topWindow,width=12)
+        self.startingWeightLabel = tkinter.Label(self.topWindow, text="Starting weight (kg): ")
+        self.startingWeightEntry = tkinter.Entry(self.topWindow, width=12)
         
         #Calorie Cut/Gain
         self.calorieCutLabel = tkinter.Label(self.topWindow, text="Calorie Cut/Gain:")
@@ -39,7 +40,7 @@ class CalorieCalulator:
         
         #Buttons
         self.applyBtn = tkinter.Button(self.buttonWindow, text="Apply", command=self.apply)
-        self.calculateBtn = tkinter.Button(self.buttonWindow, text="Calculate")
+        self.calculateBtn = tkinter.Button(self.buttonWindow, text="Calculate", command=self.calculate)
         
         #Packing windows
         self.topWindow.pack(pady=10, padx=10, anchor="w")
@@ -72,7 +73,7 @@ class CalorieCalulator:
         tkinter.mainloop()
     
     def apply(self):
-        calorieCutPerDay = 500
+        calorieCutPerDay = float(self.calorieCutEntry.get())
         weightChangePerMonth = 4
         initialWeight = float(self.startingWeightEntry.get())
         self.weightProgress.append(initialWeight)
@@ -82,11 +83,31 @@ class CalorieCalulator:
             elif self.userWeightPlanChoice.get() == 2:
                 initialWeight -= (float(self.calorieCutEntry.get()) / calorieCutPerDay) * weightChangePerMonth
             self.weightProgress.append(initialWeight)
-        print(self.weightProgress)
+        tkinter.messagebox.showinfo("Information", "Changes applied!")
+    
+    def kiloToPound(self, weight):
+        return weight * 2.2
     
     def calculate(self):
-        pass
+        resultsWindow = tkinter.Toplevel(self.mainWindow)
+        resultsWindow.title("Results")
+        resultsListBox = tkinter.Listbox(resultsWindow, width=40)
+        scrollBar = tkinter.Scrollbar(resultsWindow)
+        resultsListBox.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+        scrollBar.pack(side=tkinter.RIGHT, fill=tkinter.BOTH)
+        for i in range(len(self.weightProgress)):
+            if self.inKiloVar.get() == 1 and self.inPoundVar.get() == 1:
+                resultsListBox.insert(i, "Weight at the end of month "
+             + str(i + 1) + ": " + format(self.weightProgress[i], "0.2f") + "kg" +
+             " or " + format(self.kiloToPound(self.weightProgress[i]), "0.2f") + "lbs")
+            elif self.inKiloVar.get() == 1:
+                resultsListBox.insert(i, "Weight at the end of month "
+             + str(i + 1) + ": " + format(self.weightProgress[i], "0.2f") + "kg")
+            else:
+                resultsListBox.insert(i, "Weight at the end of month "
+             + str(i + 1) + ": " +format(self.kiloToPound(self.weightProgress[i]), "0.2f") + "lbs")
 
-
+        resultsListBox.config(yscrollcommand = scrollBar.set, bg="lightgray",border=0)
+        scrollBar.config(command = resultsListBox.yview)
 if __name__ == '__main__':
-    CalorieCalulator()
+    CalorieCalulator()  
